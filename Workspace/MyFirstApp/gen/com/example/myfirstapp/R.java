3179cb09c67af342697f837e8871ef2947929df8
wisingd <daniel.wising@gmail.com>
2014-04-20 13:48:27 +0200

    Vår första app
    så jävla cool

--- Workspace/MyFirstApp/gen/com/example/myfirstapp/R.java
+++ Workspace/MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -1097,12 +1097,15 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_main=0x7f030017;
-        public static final int fragment_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_display_message=0x7f030017;
+        public static final int activity_main=0x7f030018;
+        public static final int fragment_display_message=0x7f030019;
+        public static final int fragment_main=0x7f03001a;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
     }
     public static final class menu {
-        public static final int main=0x7f0c0000;
+        public static final int display_message=0x7f0c0000;
+        public static final int main=0x7f0c0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1147,8 +1150,10 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int button_send=0x7f0a0011;
         public static final int edit_message=0x7f0a0010;
         public static final int hello_world=0x7f0a000e;
+        public static final int title_activity_display_message=0x7f0a0012;
     }
     public static final class style {
         /** 
---
+++ Workspace/MyFirstApp/src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,56 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(70);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
+	}
+	
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_display_message,
+					container, false);
+			return rootView;
+		}
+	}
+
+}
