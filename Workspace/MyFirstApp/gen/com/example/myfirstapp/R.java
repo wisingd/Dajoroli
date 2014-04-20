7bbb94ecf5940fa807ace1015c0ffc2bedebd007
wisingd <daniel.wising@gmail.com>
2014-04-20 15:41:14 +0200

    mmm

--- Workspace/MyFirstApp/gen/com/example/myfirstapp/R.java
+++ Workspace/MyFirstApp/gen/com/example/myfirstapp/R.java
@@ -1106,6 +1106,7 @@ containing a value of this type.
     public static final class menu {
         public static final int display_message=0x7f0c0000;
         public static final int main=0x7f0c0001;
+        public static final int main_activity_actions=0x7f0c0002;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
